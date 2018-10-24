78b1be0ec209b124c181f465aa71dc8197f48bab
Fahadalharbi2 <43089880+Fahadalharbi2@users.noreply.github.com>
2018-10-23 20:53:58 -0500

    Add files via upload

---
+++ Fahad Cafe/index.php
@@ -0,0 +1,388 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+      
+    <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+    <link rel="stylesheet"type="text/css"href="vendors/css/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400" rel="stylesheet"> 
+    
+   <link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=sky-bold" />
+    
+    
+<style>
+#map {
+       
+       
+          width: 100%;
+    height: 630px;
+    position: relative;
+    z-index: 0;
+       }
+        
+   </style>
+    
+ <title>Fahad Cafe</title>
+ </head>
+   
+    
+<body>
+    
+    <audio autoplay>
+     <source src="Audio/Calming-harp-music.mp3" type="audio/mpeg">
+    </audio>
+    
+    <header>
+        <div class="row">
+                <ul class="main-language">
+                    <li><a href="#">English</a></li>
+                    <li><a href="#">عربي</a></li>
+                </ul>
+            
+            </div>
+        <nav>
+            
+            <div class="row">
+            <img src="resources/css/img/logo2.png" alt="Fahad Cafe" class="logo">
+                <ul class="main-nav">
+                    <li><a href="#">Order Online</a></li>
+                    <li><a href="#">Customer Reviews</a></li>
+                    <li><a href="#"> Reserved Table</a></li>
+                    <li><a href="#"> Menu</a></li>
+                    <li><a href="#"> Sign up</a></li>
+                
+                </ul>
+            
+            </div>
+        
+        </nav>
+    <div class="hero-text-box">
+        <h1><b>A Cafe Like Never Before </b></h1>
+         <a class="btn btn-full" href="#"><b>Menu</b></a>
+                <a class="btn btn-ghost" href="#"><b>Sign Up</b></a>
+        </div>
+    
+    </header>
+    
+     
+      <section class="section-features js--section-features" id="features">
+            <div class="row">
+                <h2>All you need is love and more coffee</h2>
+                <p class="long-copy">
+                    Hello, we're Fahad Cafe, your place to enjoy your time. We know you're always busy. No time for making coffe or cooking. So let us take care of that, we're really good at it, we promise!
+                </p>
+            </div>
+            
+            <div class="row">
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-infinite-outline icon-big"></i>
+                    <h3>Enjoy a variety of food and drinks</h3>
+                    <p>
+                        In Fahad Cafe there many kinds of food and drink. Never make your coffee or cook again! We really mean that. Enjoy a variety of food and drinks here in Fahad Cafe. You can also choose to order more flexibly if that's your style.
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-stopwatch-outline icon-big"></i>
+                    <h3>Reserve a table beforehand</h3>
+                    <p>
+                        We know how busy you are. So reserve your table, especially if you are a group of people. This help us to ensure that you're 100% happy.
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-nutrition-outline icon-big"></i>
+                    <h3>100% healthy</h3>
+                    <p>
+                        All our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-cart-outline icon-big"></i>
+                    <h3>Order anything</h3>
+                    <p>
+                        We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!
+                    </p>
+                </div>
+            </div>   
+        </section>
+   
+    
+    <section class="section-customer-reviews" id="reviews" >
+            <div class="row">
+                <h2>Customer Reviews</h2>
+                <p class="long-copy">
+                </p>
+            </div>
+        <div class="row">
+         <div class="col span-1-of-3 ">
+                    
+                    <h3></h3>
+                    <p>
+                        <span class="yelp-review" data-review-id="7iPu2i2yLCZIj-cGnLo5Gw" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=-q0rmCOPCgakJJ0H2mJ1mg" rel="nofollow noopener">Xenu M.</a>'s <a href="https://www.yelp.com/biz/maya-del-sol-oak-park?hrid=7iPu2i2yLCZIj-cGnLo5Gw" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/XL3kNO7mW7sZ2lLiJxvoEA" rel="nofollow noopener">Fahad Cafe</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+                    </p>
+                </div>
+         <div class="col span-1-of-3 ">
+                    
+                    <h3></h3>
+                    <p>
+                        <span class="yelp-review" data-review-id="7iPu2i2yLCZIj-cGnLo5Gw" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=-q0rmCOPCgakJJ0H2mJ1mg" rel="nofollow noopener">Xenu M.</a>'s <a href="https://www.yelp.com/biz/maya-del-sol-oak-park?hrid=7iPu2i2yLCZIj-cGnLo5Gw" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/XL3kNO7mW7sZ2lLiJxvoEA" rel="nofollow noopener">Fahad Cafe</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+                    </p>
+                </div>
+         <div class="col span-1-of-3 ">
+                    
+                    <h3></h3>
+                    <p>
+                        <span class="yelp-review" data-review-id="7iPu2i2yLCZIj-cGnLo5Gw" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=-q0rmCOPCgakJJ0H2mJ1mg" rel="nofollow noopener">Xenu M.</a>'s <a href="https://www.yelp.com/biz/maya-del-sol-oak-park?hrid=7iPu2i2yLCZIj-cGnLo5Gw" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/XL3kNO7mW7sZ2lLiJxvoEA" rel="nofollow noopener">Fahad Cafe</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a><script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script></span>
+                    </p>
+                </div>
+            </div>
+        
+        </section>
+    
+    
+    <section class="section-meals">
+        <div class="row">
+                <h2>Menu</h2>
+                <p class="long-copy">
+                </p>
+            </div>
+            <ul class="meals-showcase clearfix">
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/beans.jpg" alt="Beans Sections">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/breakfast.jpg" alt="Breakfast Sections">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/desert.jpg" alt="Desert Section">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/Iced_drink.jpg" alt="Iced_drink Section">
+                    </figure>
+                </li>
+            </ul>
+              <ul class="meals-showcase clearfix">
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/food.jpg" alt="Dinner section">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/lunch.jpg" alt="lunch section">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/hot_drink.jpg" alt="Hot drink section">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/breakfast.jpg" alt="Granola with cherries and strawberries">
+                    </figure>
+                </li>
+            </ul>
+        </section>
+        
+    
+    
+    
+
+        
+
+	
+
+<script>
+
+      function initMap() {
+        var myLatLng = {lat: 24.552613920146214, lng: 46.745415661317565};
+        var myLatLng1 = { lat:37.778966151840166, lng: -122.4225754688058};
+        var myLatLng2 = { lat:51.5286248104458, lng:-0.23871358873202553};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 2,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Riyadh'
+        });
+           var marker = new google.maps.Marker({
+          position: myLatLng1,
+          map: map,
+          title: 'San Fransisco'
+        });
+           var marker = new google.maps.Marker({
+          position: myLatLng2,
+          map: map,
+          title: 'London'
+        });
+      }
+   
+    </script>
+
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA21gTldTq0T157EcgQCa9ugbzsjceKo1o&callback=initMap">
+      </script>  
+    <div class="row">
+      
+        <div class="map-box">
+            </div>
+            <div class="row">
+                
+                <h2>We're happy to hear from you</h2>
+                <p class="long-copy">
+                </p>
+            </div>
+     <div id="map"></div>
+        
+       <div class="form-box" id="form">
+                    <div class="row">
+                        
+                       
+                        <form method="post" action="mailer-new.php" class="contact-form">
+                            <div class="row">
+                                
+                                <?php
+                                if ($_Get['success']==1){
+                                    echo"<div class=\"form-messages sucess\">
+                                Thank you! your message has been sent.
+                            </div>";
+                                }
+                                if ($_Get['success']==-1){
+                                    echo"<div class=\"form-messages error\">
+                                Opps! Something went wrong.
+                            </div>";
+                                }
+                                
+                            ?>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label for="name">Name</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <input type="text" name="name" id="name" placeholder="Your name" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label for="email">Email</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <input type="email" name="email" id="email" placeholder="Your email" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label for="find-us">How did you find us?</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <select name="find-us" id="find-us">
+                                        <option value="friends" selected>Friends</option>
+                                        <option value="search">Search engine</option>
+                                        <option value="ad">Advertisement</option>
+                                        <option value="other">Other</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label>Newsletter?</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <input type="checkbox" name="news" id="news" checked> Yes, please
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label>Drop us a line</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <textarea name="message" placeholder="Your message"></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col span-1-of-3">
+                                    <label>&nbsp;</label>
+                                </div>
+                                <div class="col span-2-of-3">
+                                    <input type="submit" value="Send it!">
+                                </div>
+                            
+                            </div>
+                           
+
+                        </form>
+                           
+                    </div>
+           
+           </div>
+    
+           
+     </div>
+        
+    
+    
+    
+    
+    
+    <footer>
+ 
+    <div class="row">
+        <div class="col span-1-of-2">
+        <ul class="footer-nav">
+            <li><a href="#">About Us</a></li>
+            <li><a href="resources/js/google%20Maps.html" target="_blank">Locations</a></li>
+            <li><a href="#">IOS App</a></li>
+            <li><a href="#">Android App</a></li>
+            </ul>
+        </div>
+        
+        <div class="col span-1-of-2">
+        <ul class="social-links">
+            <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+            <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+            <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+            </ul>
+            
+        </div>
+        
+        </div>
+        
+        <div class="row">
+            <p>
+            Copyright &copy; 2018 by Fahad Cafe. All rights reserved.
+            
+            </p>
+        
+        
+        </div>
+        
+    </footer>
+
+   
+    
+
+
+    
+ 
+    
+
+</body>
+
+
+</html>
